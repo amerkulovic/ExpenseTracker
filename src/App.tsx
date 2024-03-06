60b7da79154044b99f4afab86617d4c1cac8e93b
Amer Kulovic <amer.kulovic@yahoo.com>
2024-03-06 10:27:32 -0600

    created the input form

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
+import React, { useState } from "react";
+
 function App() {
+  let [total, setTotal] = useState(0);
+  let [enteredValue, setEnteredValue] = useState<number | string>("");
+
+  const changeTotal = () => {
+    const value = parseFloat(String(enteredValue));
+    setTotal(total + value);
+    setEnteredValue("");
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEnteredValue(e.target.value);
+  };
+
   return (
     <div>
-      <p>Hello</p>
+      <div>{total}</div>
+      <input value={enteredValue} type="text" placeholder="Enter an expense" onChange={handleInputChange} />
+      <button onClick={changeTotal}>Enter</button>
     </div>
   );
 }
