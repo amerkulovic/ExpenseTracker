2fec8ede79579f062dcfaf3e0ceec357e5f81d20
Amer Kulovic <amer.kulovic@yahoo.com>
2024-04-01 20:19:07 -0500

    added functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,57 @@
 import React, { useState } from "react";
 import ExpenseItem from "./components/ExpenseItem";
+import { ExpenseItemProps } from "./components/ExpenseItem";
+import { log } from "console";
+import exp from "constants";
+
+type Expense = {
+  id: number;
+  cost: number;
+  description: string;
+};
 
 function App() {
   let [total, setTotal] = useState(0);
-  let [expenses, setExpenses] = useState<number[]>([]);
+  let [expenses, setExpenses] = useState<Expense[]>([]);
   let [enteredValue, setEnteredValue] = useState<number | string>("");
+  let [enteredDescription, setEnteredDescription] = useState<string>("");
 
   const changeTotal = () => {
     const value = parseFloat(String(enteredValue));
     setTotal(total + value);
-    setExpenses([...expenses, value]);
+    setExpenses([...expenses, { id: Math.random(), cost: value, description: enteredDescription }]);
     setEnteredValue("");
+    setEnteredDescription("");
   };
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setEnteredValue(e.target.value);
   };
 
+  const handleDescriptionChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEnteredDescription(e.target.value);
+  };
+
+  const removeExpense = (id: number) => {
+    setExpenses(expenses.filter((expense) => expense.id !== id));
+    console.log(expenses);
+  };
+
   return (
     <div className="flex justify-center align-middle">
       <section className="flex flex-col">
         <div className="text-4xl font-bold text-center">{total}</div>
-        <section className="border-2 border-blue-300 rounded-md">
+        <section className="border-2 border-blue-300 rounded-md m-2">
           <input className="p-1" value={enteredValue} type="text" placeholder="Enter an expense" onChange={handleInputChange} />
-          <button onClick={changeTotal}>Enter</button>
+          <button className="p-1" onClick={changeTotal}>
+            Enter
+          </button>
         </section>
-        {expenses.map((expense) => (
-          <ExpenseItem cost={expense} />
+        <section className="border-2 border-blue-300 rounded-md m-2">
+          <input className="p-1" value={enteredDescription} type="text" placeholder="Enter an description(optional)" onChange={handleDescriptionChange} />
+        </section>
+        {expenses.map((expense, id) => (
+          <ExpenseItem key={id} id={id} cost={expense.cost} description={expense.description} buttonFunction={() => removeExpense(expense.id)} />
         ))}
       </section>
     </div>
--- src/components/ExpenseItem.tsx
+++ src/components/ExpenseItem.tsx
@@ -1,22 +1,27 @@
 import { FC } from "react";
 
-type ExpenseItemProps = {
+export type ExpenseItemProps = {
+  id: number;
   cost: number;
   description?: string;
+  buttonFunction: (id: number) => void;
 };
 
-const ExpenseItem: FC<ExpenseItemProps> = ({ cost, description }) => {
-  let expenseStyling: () => string = () => {
-    if (cost >= 0) {
-      return "bg-green-300";
-    } else {
-      return "bg-red-300";
-    }
-  };
+const ExpenseItem: FC<ExpenseItemProps> = ({ id, cost, description, buttonFunction }) => {
+  //   let expenseStyling = () => {
+  //     if (cost >= 0) {
+  //       return "bg-green-300";
+  //     } else {
+  //       return "bg-red-300";
+  //     }
+  //   };
   return (
-    <div className={cost >= 0 ? "flex justify-between w-1/2 bg-green-300" : "`flex justify-between w-1/2 bg-red-400"}>
+    <div className={cost >= 0 ? "flex justify-between bg-green-300" : "flex justify-between bg-red-400"}>
       <p className="font-bold text-xl">{cost}</p>
       <p>{description}</p>
+      <button className="font-extrabold text-2xl" onClick={() => buttonFunction(id)}>
+        -
+      </button>
     </div>
   );
 };
