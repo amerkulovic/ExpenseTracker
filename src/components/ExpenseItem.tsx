c964b243d57fe6715a2a73c7a124b1a3cd250f2f
Amer Kulovic <amer.kulovic@yahoo.com>
2024-04-01 22:22:33 -0500

    cleaned up code in ExpenseItem component

--- src/components/ExpenseItem.tsx
+++ src/components/ExpenseItem.tsx
@@ -8,15 +8,8 @@ export type ExpenseItemProps = {
 };
 
 const ExpenseItem: FC<ExpenseItemProps> = ({ id, cost, description, buttonFunction }) => {
-  //   let expenseStyling = () => {
-  //     if (cost >= 0) {
-  //       return "bg-green-300";
-  //     } else {
-  //       return "bg-red-300";
-  //     }
-  //   };
   return (
-    <div className={cost >= 0 ? "flex justify-between bg-green-300" : "flex justify-between bg-red-400"}>
+    <div className={`flex justify-between ${cost >= 0 ? "bg-green-300" : "bg-red-400"}`}>
       <p className="font-bold text-xl">{cost}</p>
       <p>{description}</p>
       <button className="font-extrabold text-2xl" onClick={() => buttonFunction(id)}>
